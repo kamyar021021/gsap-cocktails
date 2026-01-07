9fc68175753dba3a14ad244d6243cfc589f9cbc6
kamyar <kamyarjafari3000@gmail.com>
2026-01-07 05:11:23 -0800

    Implement the Art section

--- components/Home.tsx
+++ components/Home.tsx
@@ -6,6 +6,7 @@ import Navbar from "./Navbar";
 import Hero from "./Hero";
 import Cocktails from "./Cocktails";
 import About from "./About";
+import Art from "./Art";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -16,6 +17,7 @@ const Home = () => {
       <Hero />
       <Cocktails />
       <About />
+      <Art />
     </main>
   );
 };
